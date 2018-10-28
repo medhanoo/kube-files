450d3d7020cfbbcbee7e2a9ac17ac82158c93f18
Dhananjay <medhanoo@gmail.com>
2018-10-28 11:55:15 +0530

    webjoke dockerfike

---
+++ webjoke/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+/bin/sh /bin/joke.sh &
+
+$@
---
+++ webjoke/joke.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+while true
+do
+	sleep 0.6
+	joke=`wget --header "Accept: application/json" -qO- https://icanhazdadjoke.com/ | grep joke | cut -f2 -d ':'`
+	echo "
+	
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div {
+    background-color: lightgrey;
+    width: 300px;
+    border: 25px solid MediumSeaGreen;
+    padding: 25px;
+    margin: 25px;
+}
+</style>
+</head>
+<body>
+
+<h2>Demonstrating the Box Model</h2>
+
+<p>Collecting jokes from internet.</p>
+<p>Joke : </p>
+
+<div>${joke}</div>
+
+</body>
+</html>
+" > /usr/share/nginx/html/index.html
+
