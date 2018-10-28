801a6407ad9ebdd52d245cf5fc61347603feb8b2
Dhananjay <medhanoo@gmail.com>
2018-10-28 13:52:13 +0530

    added tini

--- webjoke/entrypoint.sh
+++ webjoke/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "stating joke daemon"
 joke.sh &
-
+echo "joke daemon started"
+echo '==================================================================='
+echo "App command"
+echo '==================================================================='
 echo $@
 $@
